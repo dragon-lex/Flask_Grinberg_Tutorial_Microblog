bce02b6eef79a11161e94185a143db969d1fd900
dpb15 <brannerchinese@gmail.com>
2014-02-10 18:53:59 -0500

    add Grinberg content; deprecate 20131218 notes

---
+++ REFERENCE/Grinberg_tutorial_downloaded_20140210/The Flask Mega-Tutorial, Part XV_ Ajax - miguelgrinberg.com_files/challenge.js
@@ -0,0 +1,14 @@
+
+var RecaptchaState = {
+    site : '6Ld5Zs4SAAAAAOY0DOi4r18bgHlEaz41qAoRf__S',
+    rtl : false,
+    challenge : '03AHJ_VusIF-o019JTUl05HBbjzPzCgclC-xEksrTqczsqmycz8BEUABoHdyS1yMGiB528VZEJb1N5nTZYUhWnaNS-KM2XdVYUXzVODC2f5pODeND_D0aJDLgFij9nl-Z_IwgvSC_isbjoMrnmWcU5451VVPNwoPS7iqetOk4LQtXP-oZo7xf_CU8',
+    is_incorrect : false,
+    programming_error : '',
+    error_message : '',
+    server : 'http://www.google.com/recaptcha/api/',
+    lang : 'en',
+    timeout : 1800
+};
+
+document.write('<scr'+'ipt type="text/javascript" s'+'rc="' + RecaptchaState.server + 'js/recaptcha.js"></scr'+'ipt>');
