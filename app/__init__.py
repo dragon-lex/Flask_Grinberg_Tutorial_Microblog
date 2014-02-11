91c0ec210551753dc51fbdfb36683a36e2e91fe5
dpb15 <brannerchinese@gmail.com>
2014-02-11 17:41:10 -0500

    add working code through Part V

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,19 @@
-# Part IV
+# Part V
 
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from config import basedir
+import os
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 from app import views, models
--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-# Part IV
+# Part V
 from app import db
 
 ROLE_USER = 0
@@ -10,6 +10,18 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     role= db.Column(db.SmallInteger, default=ROLE_USER)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return True
+
+    def get_id(self):
+        return unicode(self.id)
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
